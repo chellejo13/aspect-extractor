eae9fb55bb1387246504f8d5e2b03c60397f349c
chellejo13 <mj66@illinois.edu>
2025-04-15 10:38:18 -0500

    updated description

--- src/corpus_analysis.py
+++ src/corpus_analysis.py
@@ -3,12 +3,12 @@ This is the test file!
 This file will eventually contain the main function that allows the user to test the program.
 
 A CSV of small size with data organized as expected by the program will be provided to test its
-functionality in creating five data frames with relevant processed data. For an example of what
+functionality in creating three data frames with relevant processed data. For an example of what
 this CSV might look like, please contact me--the data is technically freely accessible on the TalkBank
 website where CHILDES corpora are available, but accessing them requires creating an account and
 downloading large files, so emailing me would probably be easier.
 
-This CSV will be read in and the user will be prompted to input the age range within the data that they
+The CSV will be read in and the user will be prompted to input the age range within the data that they
 want to process. If the age range is within the range provided in the CSV, the program should be able
 to parse the provided data and output files that contain main verbs, verb types, and tense-aspect info
 for each utterance.
